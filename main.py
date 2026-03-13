94c26eac54c0ba75102543ada1c2276b95648d1f
msahmoudr <msahmoudr@gmail.com>
2026-03-13 16:24:50 +0200

    kalam gameel

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import random
+import Quick_Sort
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -24,4 +25,26 @@ def kth_smallest(arr, k):
     return kth_smallest_helper(arr.copy, 0, n - 1, k)
 
 
+def kth_smallest_helper(arr, low, high, k):
+    """
+    mix between quick sort partition and binary search
+    """
+    # lw one element
+    if low == high:
+        return arr[low]
 
+
+    # htraga3 index element fe mkano el mazbot
+    pivot_index = Quick_Sort.randomized_partition(arr, low, high)
+
+    # Since array is 0-indexed, left size = pivot_index - low + 1
+    left_size = pivot_index - low + 1
+
+    if k == left_size:
+        return arr[pivot_index]
+    # nafs fekret el binary search
+    elif k < left_size:
+        return kth_smallest_helper(arr, low, pivot_index - 1, k)
+    else:
+        return kth_smallest_helper(arr, pivot_index + 1, high, k - left_size)
+    
