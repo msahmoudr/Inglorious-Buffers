7a23a1add5b973be7ff92b028d39a51cbe30c692
msahmoudr <msahmoudr@gmail.com>
2026-03-13 16:18:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import random
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -13,4 +13,15 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def kth_smallest(arr, k):
+    n= len(arr)
+    if k < 1 or k > len(arr):
+        raise ValueError(f"k must be between 1 and {len(arr)}")
+
+    return kth_smallest_helper(arr.copy, 0, n - 1, k)
+
+
+
